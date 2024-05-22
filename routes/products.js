c7266afdfee212b04402aa7224bc07e09d695502
donjuan136 <email/junker6.jd@gmail.com>
2024-05-22 11:28:12 +0700

    add method get,post,put,delete

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const Validator = require('fastest-validator');
-//const { Product } = require("../models")
-const {Product} = require("../models")
+const { Product } = require("../models")
+//const {Product} = require("../models")
 //const { Product } = require("../models/Product.js")
 const v = new Validator();
+
+router.get('/',async(req,res) =>{
+    const products = await Product.findAll();
+    return res.json(products)
+});
+
+router.get('/:id',async(req,res) =>{
+    const id = req.params.id;
+    const product = await Product.findByPK(id);
+    return res.json(product||{})
+});
 
 router.post('/', async(req, res) =>{
     const schema ={
@@ -18,12 +29,43 @@ router.post('/', async(req, res) =>{
         return res.status(400).json(validate);
     }
     //res.send('ok')
-    const product = await Product.create(req.body);
+    const product =  await Product.create(req.body);
     res.json(product);
 });
 
-router.get('/respon', (req, res,)=>{
-    res.send(200)
+
+router.put('/:id', async (req, res,)=>{
+    const id = req.params.id;
+    let product = await Product.findByPK(id);
+    if (!product) {
+        return res.json({message:"product not found"});
+    }
+
+    const schema ={
+        name:'string|optional',
+        brand:'string|optional',
+        description:'string|optional',
+    }
+    const validate = v.validate(req.body, schema);
+   
+    if (validate.length){ 
+        return res.status(400).json(validate);
+    }
+    product =await product.update(req,body);
+    res.json(product);
   });
+
+  router.delete('/:id', async(req,res) =>{
+    const id = req.params.id;
+    const product = await Product.findByPK(id);
+    if (!product) {
+        return res.json({message:"product not found"});
+    }
+    //jika ada productnya tinggal hapus
+    await product.destroy();
+    res.json({
+        message:"product deleted"
+    })
+  })
 
 module.exports = router;
